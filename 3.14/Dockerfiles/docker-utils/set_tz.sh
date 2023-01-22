24d55d2a7f356afd06907efc3b999bfe0c33142a
Sebastian Kalicki <sebastian@kalicki.email>
2023-01-22 12:18:05 +0100

    Updated

---
+++ 3.14/Dockerfiles/docker-utils/set_tz.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Purpose: Set timezone in Alpine
+# Version: 1.0
+#
+
+#
+# Check if tzdata is installed
+#
+if [ ! -d /usr/share/zoneinfo ];
+  then
+     echo "Need to install tzdata!"
+     apk add --update tzdata
+fi
+
+
+#
+# Setting timezone
+#
+if [ -f /usr/share/zoneinfo/$1 ];
+  then
+    echo "Setting timezone to $1"
+    cp /usr/share/zoneinfo/$1 /etc/localtime
+    echo $1 > /etc/timezone
+  else
+    echo "timezone $1 not defined"
+  fi
+
+#
+# Remove tzdata, not needed anymore
+#
+apk del tzdata
+
+#
+# Remove cache
+#
+rm -rf /var/cache/apk/*
